c593cd0cd3c10a8111228e32edba7d95042e835f
danilaleksin <danilaleksin@gmail.com>
2016-10-06 16:21:44 +0300

    Add sources, gulp and public content

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+	inject = require('gulp-inject'),
+	runSequence = require('run-sequence');
+  
+gulp.task('build:styles', function () {
+  gulp.src('app/assets/stylesheets/*.css')
+      .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('build:scripts', function () {
+  gulp.src('app/assets/javascripts/*.js')
+      .pipe(gulp.dest('public/js'))
+});
+
+gulp.task('build:images', function () {
+  gulp.src('app/assets/images/*.*')
+      .pipe(gulp.dest('public/img'))
+});
+
+gulp.task('build:fonts', function () {
+  gulp.src('app/assets/stylesheets/fonts/*.*')
+      .pipe(gulp.dest('public/css/fonts'))
+});
+
+gulp.task('default', 
+	(callback) => runSequence(
+		'build:styles',
+		'build:scripts',
+		'build:images',
+		'build:fonts'
+		)
+);
