5a5bd4efce403723fbf586b9e1cf54a4aa0a0c8e
danilaleksin <danilaleksin@gmail.com>
2016-10-31 16:06:30 +0300

    Navbar products preview

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028123045) do
+ActiveRecord::Schema.define(version: 20161031110138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20161028123045) do
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
     t.string   "name"
+    t.string   "banner_url"
   end
 
 end
