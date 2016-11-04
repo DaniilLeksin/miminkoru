8f8839af854625997149371c9ebe056e990779aa
Daniil Leksin <danilaleksin@gmail.com>
2016-11-04 09:43:24 +0300

    add products view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,17 @@
 class ProductsController < ApplicationController
 	def index
-		# @products = Product.all
+		@products = Product.all
 		# @items = Item.all
+		# @categories = Category.all
 		render nothing: true
+		# render json: @products
 	end
 
 	def show
 		@items_by_product_id = Item.where(:product_id => params[:id])
 		@products_by_category = Product.where(:category_id => params[:id])
 		@products = Product.all
+		@categories = Category.all
+		@items = Item.all
     end
 end
