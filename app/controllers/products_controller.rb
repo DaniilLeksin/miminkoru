20504eff6ed768f234c940035b06be763f9d73ec
Dan Lexin <danilaleksin@gmail.com>
2016-12-01 02:32:28 +0300

    Show only active items

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,15 @@ class ProductsController < ApplicationController
 		@product_category = @product.category_id
 		# @products_by_category = Product.where(:category_id => params[:id])
 		@items_by_product_id = Item.where(:product_id => params[:id], :category_id => @product_category)
-		
+		# TODO: need to hide not active items
+		@showed_items_by_category_id = []
+		@items_by_product_id.each do |item|
+			@item_dimensions = Dimension.where(:item_id => item.id, :status => 'active')
+			if @item_dimensions.count > 0
+				@showed_items_by_category_id.append(item)
+			end
+		end
+
 		@products = Product.all
 		@categories = Category.all
 		@items = Item.all
